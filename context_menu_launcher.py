3f038f0b0b1dd072cb60970c2a259462cab4de1e
ironcpa <ironcpa@gmail.com>
2017-11-24 11:57:34 +0900

    collect_db mod : last commit's comment is this  - consider removing update cuz all target disk or dir's rows are deleted before insert/update

--- context_menu_launcher.py
+++ context_menu_launcher.py
@@ -47,20 +47,3 @@ if __name__ == '__main__':
                 path = path + os.path.sep
             collect_files_to_db(disk_name, path)
 
-        # print(sys.argv[2])
-        # path = sys.argv[2]
-        # disk_name = None
-        # if len(path) == 3:
-        #     disk_name = win32api.GetVolumeInformation(os.path.splitdrive(path)[0])
-        # print('disk', disk_name)
-        # db = db_util.DB()
-        # if disk_name and co.is_disk_online(disk_name):
-        #     db.delete_by_disk(disk_name)
-        #     fileinfos = find_file.find_file(path, '')
-        #     db.update_product_w_fileinfos(fileinfos)
-        # else:
-        #     db.delete_by_dir(path)
-        #     fileinfos = find_file.find_file(path, '')
-        #     db.update_product_w_fileinfos(fileinfos)
-
-
